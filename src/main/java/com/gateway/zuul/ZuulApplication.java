075040bb4e554e767d7f46da8bc5c00bd015d3b7
murtaza896 <murtaza896@gmail.com>
2020-12-18 19:50:01 +0530

    created POST filter

--- src/main/java/com/gateway/zuul/ZuulApplication.java
+++ src/main/java/com/gateway/zuul/ZuulApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
 
+//@CrossOrigin(origins = "http://localhost:4200/" , allowCredentials = "true")
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -14,10 +18,17 @@ public class ZuulApplication {
 	@Autowired
 	private ZuulRequestFilter zuulRequestFilter;
 	
+	@Autowired
+	private ZuulPostRequestFilter zuulPostRequestFilter;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 	
-	
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
 
 }
--- src/main/java/com/gateway/zuul/ZuulRequestFilter.java
+++ src/main/java/com/gateway/zuul/ZuulRequestFilter.java
@@ -1,10 +1,19 @@
 package com.gateway.zuul;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -12,26 +21,55 @@ import com.netflix.zuul.exception.ZuulException;
 @Component
 public class ZuulRequestFilter extends ZuulFilter {
 
+	@Autowired 
+	private EurekaClient eurekaClient;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@Override
 	public boolean shouldFilter() {
 		// TODO Auto-generated method stub
-		return true;
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest req = ctx.getRequest();
+		
+		String reqURI = req.getRequestURI();
+		return !(reqURI.startsWith("/auth") || reqURI.startsWith("/user"));
 	}
-
+	
 	@Override
 	public Object run() throws ZuulException {
 		// TODO Auto-generated method stub
 		RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
+        HttpServletRequest req = ctx.getRequest();
 
-        String apiKey = request.getHeader("X-API-KEY");
-        System.out.println(apiKey);
-        if (apiKey.equals("bad")){
+    	InstanceInfo instance = eurekaClient.getNextServerFromEureka("AUTH-SERVICE", false);
+		
+		String hosturi = instance.getHomePageUrl(); //http://localhost:8080
+		
+		HttpHeaders headers = new HttpHeaders();
+		ResponseEntity<String> response = null;
+		try
+		{
+			for(Cookie x: req.getCookies())
+				headers.add("Cookie", x.getName() + "=" + x.getValue());
+			HttpEntity<String> entity = new HttpEntity<String>(headers);
+			response = restTemplate.exchange(hosturi + "user/get-user", HttpMethod.GET, entity, String.class);
+		}
+		catch(Exception e)
+		{
+			System.out.println("No cookie found");
+		}
+		
+		
+		
+		if (response == null || response.getStatusCode().compareTo(HttpStatus.BAD_REQUEST) == 0)
+        {
             // blocks the request
             ctx.setSendZuulResponse(false);
 
             // response to client
-            ctx.setResponseBody("API key not authorized");
+            ctx.setResponseBody("Not authorized");
             ctx.getResponse().setHeader("Content-Type", "text/plain;charset=UTF-8");
             ctx.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
         }
